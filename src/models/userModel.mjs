ea24181b04f00ff88b022f39fb8f14eda7da18ba
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-05 17:14:25 +0800

    feat: add password field

--- src/models/userModel.mjs
+++ src/models/userModel.mjs
@@ -7,6 +7,7 @@ const userSchema = new Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true},
     username: { type: String, required: true },
+    password: { type: String, required: true },
     membership: {
         type: String,
         required: true,

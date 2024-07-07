93dbfd0397d0e81abe9c00a779756482948cf64b
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-07 20:39:51 +0800

    chore: archive

---
+++ src/helpers/archive/authenticate.mjs
@@ -0,0 +1,25 @@
+import passport from 'passport';
+import AuthenticateError from '../errors/authError.mjs';
+
+const authenticate = (req, res, next) => passport.authenticate('local', (err, user, info) => {
+    if(err) {
+        next(err);
+        return;
+    }
+
+    if(!user) {
+        const error = new AuthenticateError('Authentication failed', info.message)
+
+        next(error)
+        return;
+    }
+
+    req.user = user.toJSON();
+    
+    next();
+});     
+
+
+export default {
+    authenticate
+};

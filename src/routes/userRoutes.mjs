e9ab241892ec591ee828daf08baa8e90865f3182
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-07 19:37:41 +0800

    refactor: route protections

--- src/routes/userRoutes.mjs
+++ src/routes/userRoutes.mjs
@@ -1,22 +1,23 @@
 import { Router } from 'express';
 import UserController from '../controllers/user/index.mjs';
-import attachToken from '../middlewares/attachToken.mjs';
 import verifyToken from '../middlewares/verifyToken.mjs';
 import isAdmin from '../middlewares/isAdmin.mjs';
 import isCurrentUser from '../middlewares/isAdminOrCurrentUser.mjs';
+import validObjectId from '../middlewares/isObjectId.mjs';
+
 
 const route = Router();
 
-route.get('/', [attachToken, verifyToken, isAdmin], UserController.GET.users);
-route.get('/:userId', [attachToken, verifyToken],UserController.GET.users_detail);
+route.get('/', verifyToken, isAdmin, UserController.GET.users);
+route.get('/:userId', verifyToken, validObjectId('userId'), UserController.GET.users_detail);
 
 route.post('/', UserController.POST.users_signup);
 route.post('/tokens', UserController.POST.users_login);
 // TODO: IMPLEMENT LIKE COMMENTS
 // route.post('/:userId/:commentId', UserController.POST.users_like_comment);
 
-route.put('/:userId', [attachToken, verifyToken, isCurrentUser], UserController.PUT.users_update);
+route.put('/:userId', verifyToken, [validObjectId('userId'), isCurrentUser], UserController.PUT.users_update);
 
-route.delete('/:userId', [attachToken, verifyToken, isCurrentUser], UserController.DELETE.users_delete);
+route.delete('/:userId', verifyToken, [validObjectId('userId'), isCurrentUser], UserController.DELETE.users_delete);
 
 export default route;

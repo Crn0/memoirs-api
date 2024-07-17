bdb30fa05fc4d7aed198eb7b9a3337a0f62b7f56
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-17 14:38:02 +0800

    feat: handle temp files

--- src/bin/www.mjs
+++ src/bin/www.mjs
@@ -1,10 +1,12 @@
 import http from 'http';
 import path from 'path';
 import debugFunc from 'debug';
+import fs from 'fs/promises'
 import app from '../app.mjs';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { PORT } from '../constants/env.mjs';
+import { unlink } from 'node:fs/promises';
 
 const __dirname =
     import.meta.dirname || dirname(fileURLToPath(import.meta.url));
@@ -23,10 +25,30 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
+
+const cleanupImage = async () => {
+    const res = await fs.readdir(path.join(__dirname, '..', 'temp', 'images'))
+
+    res.map(async (fileName) => {
+        const filePath = `${path.join(__dirname, '..', 'temp', 'images')}/${fileName}`;
+        console.log(filePath)
+        await unlink(filePath)
+    })
+}
+/** 
+ *  remove the uploaded image when the server closed
+ */ 
+[`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`, `SIGTERM`].map((eventType) => {
+    process.on(eventType, async () => {
+        await cleanupImage()
+
+        process.exit(0);
+    })
+});
 /**
  * Listen on provided port, on all network interfaces.
  */
-
+  
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);

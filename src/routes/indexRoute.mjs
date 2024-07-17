9f3363fe6e866ef112450835060c62eca98b47b5
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-17 14:38:15 +0800

    feat: add tag route

--- src/routes/indexRoute.mjs
+++ src/routes/indexRoute.mjs
@@ -1,10 +1,12 @@
 import UserRoute from './userRoutes.mjs';
 import PostRoute from './postRoutes.mjs';
 import CommentRoute from './commentRoute.mjs';
+import TagRoute from './tagRoute.mjs'
 
 
 export {
     UserRoute,
     PostRoute,
     CommentRoute,
+    TagRoute,
 };

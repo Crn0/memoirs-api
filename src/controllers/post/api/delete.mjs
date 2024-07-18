f79e1cfc256bf70b2d11e0e81f02cc0aaab78da7
Crno <118383162+Crn0@users.noreply.github.com>
2024-07-18 13:09:30 +0800

    feat: throw error on delete if resource not exist

--- src/controllers/post/api/delete.mjs
+++ src/controllers/post/api/delete.mjs
@@ -3,6 +3,7 @@ import asyncHandler from 'express-async-handler';
 import Post from '../../../models/postSchema.mjs';
 import Cloudinary from '../../../helpers/media/cloudinary.mjs';
 import httpStatusCode from '../../../constants/httpStatusCode.mjs';
+import APIError from '../../../helpers/errors/apiError.mjs';
 
 const posts_delete = asyncHandler(async (req, res, _) => {
     const { postId } = req.params;
@@ -13,8 +14,12 @@ const posts_delete = asyncHandler(async (req, res, _) => {
     });
 
     if (post === null) {
-        res.status(httpStatusCode.NO_CONTENT).json({});
-        return;
+        throw new APIError(
+            'post does not exist',
+            'NOT FOUND',
+            'RESOURCE ERROR',
+            httpStatusCode.NOT_FOUND
+        );
     }
 
     await Cloudinary.destroy(post.cover.cloudinary_id);

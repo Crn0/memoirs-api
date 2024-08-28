3b25f2d1be35904aef17b5ce96082d19cf9f7ec6
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-29 02:22:32 +0800

    feat: remove localhost as origin on prod

--- src/configs/cors.mjs
+++ src/configs/cors.mjs
@@ -1,7 +1,13 @@
 import 'dotenv/config'
 
+const origins = ['http://localhost:5173', process.env.FRONTEND_VIEWER, process.env.FRONTEND_CMS];
+
+if (process.env.NODE_ENV !== 'development') {
+    origins.shift();
+}
+
 const corsOptions = {
-    origin: ['http://localhost:5173', process.env.FRONTEND_VIEWER, process.env.FRONTEND_CMS],
+    origin: origins,
     methods: 'GET,HEAD,PUT,POST,DELETE',
     optionsSuccessStatus: 200
 }
